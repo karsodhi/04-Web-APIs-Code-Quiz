f41d7fce7cf93a8731915907a46450dc28f1af7e
Kar Sodhi <karsodhi1212@gmail.com>
2023-02-21 15:08:18 -0600

    added new javascript and css

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 04-Web-APIs-Code-Quiz
+# 04-Web-APIs-Code-Quiz 
+Coding Quiz
+
+# 04-homework
+This is the fourth homework challenge that was presented to us.  We were to create a multiple choice quiz using HTML, CSS, and javascript.  When an answer is chosen points are added and they are presented a new question.  When an incorrect answer is chosen 5 second runs off the time that they are given.  In the end high scores are documented and placed on an high scores page.  The quiz is 5 questions long.
+
+
+
+# Mentions
+I worked with a tutor for this assignment through the tutoring session for help and guidance as well as the TA's help from class.
+
+# Screenshots
+
+![This is a multiple choice coding quiz](./assets/images/quiz.jpg)
---
+++ assets/script.js
@@ -0,0 +1,127 @@
+var questions = document.querySelector("#questions");
+var timerHolder = document.querySelector("#timerHolder");
+var answers = document.querySelector("#answers");
+var submit = document.querySelector("#submit");
+var start = document.querySelector("#start");
+var initials = document.querySelector("#initials");
+var feedback = document.querySelector("#feedback");
+var counterTimer;
+var timer;
+var score = 0
+var questionsArray = [
+    {
+        question:"Arrays in Javascript can be used to store", 
+        choices:["numbers", "strings", "booleans", "all the above",],
+        correctAnswer:"all the above"
+    },
+        {
+            question:"Which method returns the length of a string", 
+            choices:["length()", "size()", "index()", "array()",],
+            correctAnswer:"length()"
+        },
+    
+        {
+            question:"Inside which element do you put Javascript?", 
+            choices:["<var>", "<section>", "<footer>", "<script>",],
+            correctAnswer:"<script>"
+        },
+    
+        {
+            question:"What is a element that represents TRUE or FALSE values", 
+            choices:["boolean", "array", "string", "condition",],
+            correctAnswer:"boolean"
+        },
+    
+        {
+            question:"The condition in an if/else statement is enclosed with", 
+            choices:["curley brackets", "square brackets", "parenthesis", "quotation marks",],
+            correctAnswer:"parenthesis"
+        },
+    
+]
+function startTime() {
+    timer=setInterval(function(){
+counterTimer--;
+timerHolder.textContent=counterTimer;
+if (counterTimer === 0){
+    clearInterval(timer)
+    // invoke function that loses the game. lose game()
+    loseGame()
+}
+    },1000)
+}
+
+function minusTimer() {
+    counterTimer-=5
+}
+function loseGame() {
+    localStorage.setItem("currentScore",score)
+    // This is where I return my user to the end of the HTML (localstore.getitem)
+}
+
+start.addEventListener("click",startQuiz)
+function startQuiz(){
+    counterTimer=60;
+var startButton = document.getElementById("startDiv")
+startButton.style.display="none"
+startTime()
+showQuestion1()
+}
+function showQuestion1(){
+    var questionHolder = document.getElementById("question1")
+    var question = document.createElement("h2")
+    question.textContent=questionsArray[0].question
+    questionHolder.prepend(question)
+    // below is list items aka choices for question
+    var choices=document.getElementById("choices1")
+    for (var i=0;i<questionsArray[0].choices.length;i++){
+        var listItem=document.createElement("li")
+        listItem.setAttribute("id",questionsArray[0].choices[i])
+        listItem.textContent=questionsArray[0].choices[i]
+        choices.append(listItem)
+
+        listItem.addEventListener("click",gradeAnswer)
+    }
+    //check which choice user picked and do something
+    function gradeAnswer(e){
+        if(e.target.id === questionsArray[0].correctAnswer){
+            score += 10 //add whatever score
+            // User selected the correct answer 
+            console.log("Correct!");
+            showQuestion2()
+        } else {
+            showQuestion2()
+            minusTimer()
+            // user selected the wrong answer
+            console.log("Wrong!");
+        }
+    }
+
+    function showQuestion2(){
+        var questionHolder = document.getElementById("question2")
+        var question = document.createElement("h2")
+        question.textContent=questionsArray[1].question
+        questionHolder.prepend(question)
+        // below is list items aka choices for question
+        var choices=document.getElementById("choices1")
+        for (var i=0;i<questionsArray[1].choices.length;i++){
+            var listItem=document.createElement("li")
+            listItem.setAttribute("id",questionsArray[1].choices[i])
+            listItem.textContent=questionsArray[1].choices[i]
+            choices.append(listItem)
+    
+            listItem.addEventListener("click",gradeAnswer)
+        }
+        //check which choice user picked and do something
+        function gradeAnswer(e){
+            if(e.target.id === questionsArray[1].correctAnswer){
+                score += 10 //add whatever score
+                showQuestion3()
+            } else {
+                showQuestion3()
+                minusTimer()
+            }
+        }
+    }
+}
+
